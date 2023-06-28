47d4e42005dfd47bc0013c0775267437dab5812d
yolovi <yolandalopevi@gmail.com>
2023-06-28 15:35:36 +0200

    ejercicio terminado

---
+++ e1.js
@@ -0,0 +1,23 @@
+// Ejercicio 1
+// Crear un archivo con el nombre e1.js
+// Levantar un servidor utilizando Express, al levantar el servidor tiene que mostrar un mensaje que diga: 
+// `Servidor levantado en el puerto ${puerto}`.
+
+//1.IMPORTACIONES -----------------------------
+const express = require("express");
+const app = express()
+
+const puerto = 3000
+
+//2. MIDDLEWARES -------------------------------
+
+
+//3. ENDPOINTS --------------------------------
+
+
+
+//4. LISTEN PUERTO ----------------------------
+app.listen(3000, () => {
+    console.log(`Servidor levantado en el puerto ${puerto}`)
+})
+
